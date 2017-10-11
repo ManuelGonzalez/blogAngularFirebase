8bb5f78e31ecdc5b6d5194f7fa23dbd929432c7a
Manuel Gonzalez <manu.gz.15.16@gmail.com>
2017-10-11 16:55:09 -0300

    creacion de rutas y otros componentes para darle dinamismo al blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PostComponent } from './components/post/post.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const appRoutes:Routes = [
+  {
+    path: '',
+    component: BodyComponent
+  },
+  {
+    path: 'home',
+    component: BodyComponent
+  },
+  {
+    path: 'post',
+    component: PostComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children:[
+      /*
+      {
+        path: ':name',
+        component: UserComponent
+      }
+      */
+    ]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    PostComponent,
+    DashboardComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
